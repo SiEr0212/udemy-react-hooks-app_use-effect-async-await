2e526347aa550f017d52f93ea738250a13dbd195
SiEr0212 <simonertel@gmx.de>
2021-05-03 23:18:47 +0200

    added get request to github api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import {  useState, useEffect} from "react";
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const initProfile = {
+  followers: null,
+  publicRepos: null,
+};
 
 function App() {
+  const [profile, setProfile] = useState(initProfile);
+
+  const getProfile = async () => {
+    const response = await fetch("https://api.github.com/users/SiEr0212");
+    const json = await response.json();
+
+    setProfile({
+      followers: json.followers,
+      publicRepos: json.public_repos
+
+    })
+
+    console.log(initProfile)
+    
+  };
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-      
+        <h2>Fetch Data</h2>
+        <h3>{}</h3>
       </header>
     </div>
   );
